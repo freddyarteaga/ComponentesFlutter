e1289daf5a9babc0f49ab86a0fbfe31b18d5e278
Freddy <freddy.artea@gmail.com>
2020-10-19 23:43:43 -0500

    Image y FadeIn Image

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -7,8 +7,11 @@ class CardPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Cards'),
       ),
-      body: ListView(
-          padding: EdgeInsets.all(10.0), children: <Widget>[_cardTipo1()]),
+      body: ListView(padding: EdgeInsets.all(10.0), children: <Widget>[
+        _cardTipo1(),
+        SizedBox(height: 30.0),
+        _cardTipo2()
+      ]),
     );
   }
 
@@ -38,5 +41,28 @@ class CardPage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Widget _cardTipo2() {
+    return Card(
+        child: Column(
+      children: <Widget>[
+        FadeInImage(
+          image: NetworkImage(
+              'https://cdn2.wanderlust.co.uk/media/1028/cropped-shutterstock_497799013.jpg?anchor=center&mode=crop&width=1440&height=540&format=auto&quality=80&rnd=131915974290000000'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+          height: 300.0,
+          fit: BoxFit.cover,
+        ),
+        // Image(
+        //   image: NetworkImage(
+        //       'https://cdn2.wanderlust.co.uk/media/1028/cropped-shutterstock_497799013.jpg?anchor=center&mode=crop&width=1440&height=540&format=auto&quality=80&rnd=131915974290000000'),
+        // ),
+        Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('No tengo idea que poner'))
+      ],
+    ));
   }
 }
