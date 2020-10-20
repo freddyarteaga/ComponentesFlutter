1d7bdc387c00032a2317d17476825302239d99bd
Freddy <freddy.artea@gmail.com>
2020-10-19 22:12:22 -0500

    rutas generadas dinamicamente

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:componentes/src/routes/routes.dart';
 
-import 'package:componentes/src/pages/home_page.dart';
 import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
 // import 'package:componentes/src/pages/home_temp.dart';
 
 void main() => runApp(MyApp());
@@ -17,10 +16,12 @@ class MyApp extends StatelessWidget {
 
       // definimos rutas
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/': (BuildContext context) => HomePage(),
-        'alert': (BuildContext context) => AlertPage(),
-        'avatar': (BuildContext context) => AvatarPage(),
+      routes: getApplicationRoutes(),
+
+      onGenerateRoute: (RouteSettings settings) {
+        print('ruta llamada: ${settings.name}');
+        return MaterialPageRoute(
+            builder: (BuildContext context) => AlertPage());
       },
     );
   }
