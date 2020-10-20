343d56acc1466072161faa8c2fa118e0d2535f62
Freddy <freddy.artea@gmail.com>
2020-10-19 21:56:41 -0500

    navegar con rutas con nombre

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
 // import 'package:componentes/src/pages/home_temp.dart';
 
 void main() => runApp(MyApp());
@@ -9,8 +11,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Componentes App',
-        home: HomePage());
+      debugShowCheckedModeBanner: false,
+      title: 'Componentes App',
+      // home: HomePage());
+
+      // definimos rutas
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => HomePage(),
+        'alert': (BuildContext context) => AlertPage(),
+        'avatar': (BuildContext context) => AvatarPage(),
+      },
+    );
   }
 }
