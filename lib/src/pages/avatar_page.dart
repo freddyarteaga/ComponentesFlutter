ab530caf7c59321575dfc92ab5411a4cc5d9a824
Freddy <freddy.artea@gmail.com>
2020-10-27 09:43:45 -0500

    Widget Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -1,3 +1,5 @@
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 class AvatarPage extends StatelessWidget {
@@ -6,7 +8,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  'https://www.pcgamesn.com/wp-content/uploads/2018/10/all-dead-by-daylight-killers-best-900x507.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
       ),
+      body: Center(
+          child: FadeInImage(
+        image: NetworkImage(
+            'https://bloody-disgusting.com/wp-content/uploads/2020/01/dead-by-daylight-toy.png'),
+        placeholder: AssetImage('assets/jar-loading.gif'),
+        fadeInDuration: Duration(milliseconds: 200),
+      )),
     );
   }
 }
