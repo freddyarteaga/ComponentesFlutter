298ec368f570b9fe3ea4cee6487cd859c5284d8d
Freddy <freddy.artea@gmail.com>
2020-10-28 23:26:14 -0500

    Slider

---
+++ lib/src/pages/slider_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class SliderPage extends StatefulWidget {
+  @override
+  _SliderPageState createState() => _SliderPageState();
+}
+
+class _SliderPageState extends State<SliderPage> {
+  double _valorSlider = 100.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Slider'),
+        ),
+        body: Container(
+            padding: EdgeInsets.only(top: 50.0),
+            child: Column(
+              children: <Widget>[_crearSlider(), crearImagen()],
+            )));
+  }
+
+  _crearSlider() {
+    return Slider(
+      activeColor: Colors.indigoAccent,
+      label: 'Tamano de la imagen',
+      // divisions: 20,
+      value: _valorSlider,
+      min: 10.0,
+      max: 400.0,
+      onChanged: (valor) {
+        setState(() {
+          _valorSlider = valor;
+        });
+      },
+    );
+  }
+
+  crearImagen() {
+    return Image(
+      image: NetworkImage(
+          'https://mobile.deadbydaylight.com/wp-content/uploads/2020/04/The-shape-hero-mobile.jpg'),
+      width: _valorSlider,
+      fit: BoxFit.contain,
+    );
+  }
+}
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,3 +1,4 @@
+import 'package:componentes/src/pages/slider_page.dart';
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/pages/home_page.dart';
@@ -15,5 +16,6 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'card': (BuildContext context) => CardPage(),
     'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
     'inputs': (BuildContext context) => InputPage(),
+    'sliders': (BuildContext context) => SliderPage(),
   };
 }
